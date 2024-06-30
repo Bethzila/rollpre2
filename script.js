f2d3bee466452ccb63788068e6cc0f6b79599c6f
Beth Melo <144193493+Bethzila@users.noreply.github.com>
2024-06-30 20:32:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*carrossel*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+/*cep*/
+
+function myFunction()
+{
+var x;
+
+var cep=prompt("Digite seu cep:");
+
+if (cep!=null)
+  {
+  x="Frete Gratis para: " + cep ;
+  document.getElementById("demo").innerHTML=x;
+  }
+}
+
